56755832d8da6a145990959a1433a39b8cc7b2ee
fulcrumAle <134450730+fulcrumAle@users.noreply.github.com>
2023-12-04 11:35:01 -0400

    subiendo modelado

---
+++ Models/Avion.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoBackendAlejandro.Models
+{
+    public class Avion
+    {
+        [Key]
+        public int idAvion { get; set; }
+        public string Modelo { get; set; }
+        public double Peso { get; set; }
+        public int Capacidad { get; set; }
+        public int idHangar { get; set; }
+        public int idPiloto { get; set; }
+    }
+}
---
+++ Models/Piloto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoBackendAlejandro.Models
+{
+    public class Piloto
+    {
+        [Key]
+        public int Nro_Licencia { get; set; }
+        public string Restricciones { get; set; }
+        public double Salario { get; set; }
+        public string Turno { get; set; }
+
+    }
+}
